b0db2c88ea556a6ce2b469103e692b8ea7ecac04
Thivina Samarakkody <thivinasamarakkody@gmail.com>
2024-08-29 15:50:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("byeeeeeeeee");
+// console.log("byeeeeeeeee");
+//pass = NaY6fudHCxWxuann
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next) => {
+    res.send("It is working..............");
+})
+
+mongoose.connect("mongodb+srv://thivinasamarakkody:dcba4321@mern-wastemgt.jjkh7.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000)
+})
+.catch((err) => console.log(err));
